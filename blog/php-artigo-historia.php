28b837084578062267cd9974187b4c051a1cc46e
NicolasFischer2002 <79231180+NicolasFischer2002@users.noreply.github.com>
2021-02-17 19:53:52 -0300

    Add files via upload

---
+++ blog/php-artigo-historia.php
@@ -0,0 +1,11 @@
+<?php
+
+$artigo3 = [
+
+    'titulo' => 'Galileu Galileu foi mesmo perseguido pela Igreja Católica?',
+    'conteudo' => 'Para concluir, o comunismo possui seu objetivo final aparentemente nobre, porém o caminho que é preciso seguir para chegar até lá é longo, desgastante, caro, violento e impossível, na visão de um cético e realista. Já que para chegar a esse grande momento, seria necessário renunciar quase tudo que existe e embarcar em um barco de mortes e invenções sem fim. Abandonar individualismo, propriedade privada, família, Deus, liberdade etc. Eu não creio em mundo perfeito e em útopias, eu acredito que o mundo é imperfeito sim, porém não cabe ao estado ou a uma revolução organizar tudo, dizer como tudo deve ser feito. Toda mudança começa em nós. parafraseando Gandalf "Saruman acredita que apenas um grande poder pode manter o mal sobre controle, mas não é o que descobri. Descobri que são as pequenas coisas, as tarefas diárias de pessoas comuns que mantém o mal afastado, simples ações de bondade e amor" e Edmund Burke:"Aqueles que tentam nivelar nunca igualam. Em todas as sociedades compostas de diferentes classes de cidadãos é necessário que algumas delas se sobreponham às outras. Os niveladores, portanto, apenas mudam e pervertem a ordem natural das coisas, sobrecarregando o edifício social ao colocar no ar o que a solidez do edifício exige que seja posto no chão".',
+    'link' => 'artigo-politica.php',
+  
+  ];
+
+?>
