b15cb0eaa1f245420b7799aa197ccf50986d8a36
NicolasFischer2002 <79231180+NicolasFischer2002@users.noreply.github.com>
2021-03-23 19:13:58 -0300

    Delete blog directory

--- blog/bdcomentarios.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE TABLE `tbcomentarios` ( 
-`id` INT( 5 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-`nome` VARCHAR( 30 ) NOT NULL ,
-`email` VARCHAR( 40 ) NOT NULL ,
-`data` DATE NOT NULL ,
-`comentario` VARCHAR( 255 ) NOT NULL 
-) ;
